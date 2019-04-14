9d0c4b6264de27f1e7d894aa300fc93bc8a7ac89
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-04-13 22:00:29 -0500

    got the newly created buttons to stop repeating themselves

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -5,9 +5,65 @@ var topics = [
   // "School Rumble",
   // "My Hero Academia",
   // "Attack on Titan"
+  // failed attempt to make buttons using the var; the var is used to make buttons
 ];
 
+$("button").on("click", function() {
+  var topic = $(this).attr("data-anime");
+  console.log(this);
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    topic +
+    "&api_key=8jb7rE8JNIYpKtgXawpdsV8v4iqNrv84" +
+    "&limit=10";
+  function makeImage(obj) {
+    //Code I tried to use to play the gifs on click; would not load gifs after implemented
+    //   var play = `<div>
+    //   <p>Rating: ${obj.rating}</p>
+    //   <img src="${obj.images.fixed_height.url}" />
+    // </div>
+    // `;
+    //   var still = `<div>
+    //   <p>Rating: ${obj.rating}</p>
+    //   <img src="${obj.images.fixed_height_still.url}" />
+    // </div>
+    // `;
+    //   event.preventDefault();
+    //   //build the shell of the function see below
+    //   return `
+    //     <div>
+    //       <p>Rating: ${obj.rating}</p>
+    //       <img src="${obj.images.fixed_height_still.url}" />
+    //     </div>
+    //     `;
+
+    //   function playGif() {
+    //     if (still) {
+    //       play;
+    //     } else if (play) {
+    //       still;
+    //     }
+    //   }
+    return `
+      <div>
+        <p>Rating: ${obj.rating}</p>
+        <img src="${obj.images.fixed_height.url}" />
+      </div> 
+      `;
+      //use this to have a paused image <img src="${obj.images.fixed_height_still.url}" />
+  }
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    $("#gifs-appear-here").prepend(response.data.map(makeImage));
+  });
+});
+
 function renderButtons() {
+  $("#buttons-view").empty();
+  //NEED THIS TO PREVENT BUTTONS FROM REPEATING
   for (var i = 0; i < topics.length; i++) {
     var a = $("<button>");
 
@@ -24,42 +80,6 @@ $("#add-anime").on("click", function(event) {
 
   topics.push(anime);
   console.log(topics);
+  console.log(anime);
   renderButtons();
-});
-
-$("button").on("click", function() {
-  var topic = $(this).attr("data-anime");
-  console.log(this);
-  var queryURL =
-    "https://api.giphy.com/v1/gifs/search?q=" +
-    topic +
-    "&api_key=8jb7rE8JNIYpKtgXawpdsV8v4iqNrv84" +
-    "&limit=10";
-  function makeImage(obj) {
-    //build the shell of the function see below
-    return `
-<div>
-  <p>Rating: ${obj.rating}</p>
-  <img src="${obj.images.fixed_height_still.url}" />
-</div> 
-`;
-    //obj.images.fixed_height_still.url
-    //function makeImage(obj){
-    // return `
-    // <div>
-    // <p>Rating: R</p>
-    //  <img src="" />
-    // </div>
-    // `
-  }
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    // Step 1: Run this file, click a button, and see what the response object looks like in the browser's console.
-    // Open up the data key, then open up the 0th, element. Study the keys and how the JSON is structured.
-
-    console.log(response);
-    $("#gifs-appear-here").prepend(response.data.map(makeImage));
-  });
 });
